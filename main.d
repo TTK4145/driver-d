a11ea51babd3237ca0c967b4b80b4df0d8f16afb
klasbo <klasbo@gmail.com>
2022-03-02 17:46:47 +0100

    Add test code to example

--- main.d
+++ main.d
@@ -12,19 +12,42 @@ void main(){
     spawn(&pollObstruction, thisTid);
     spawn(&pollStopButton,  thisTid);
 
+    auto dirn = Dirn.up;
+
     while(true){
         receive(
             (CallButton a){
                 a.writeln;
+                callButtonLight(a.floor, a.call, true);
             },
             (FloorSensor a){
                 a.writeln;
+                floorIndicator(a);
+                switch(a){
+                case 0: 
+                    dirn = Dirn.up;
+                    motorDirection(dirn);
+                    break;
+                case 3: 
+                    dirn = Dirn.down; 
+                    motorDirection(dirn);
+                    break;
+                default: break;
+                }
             },
             (Obstruction a){
                 a.writeln;
+                doorLight(a);
+                motorDirection(a ? Dirn.stop : dirn);
             },
             (StopButton a){
                 a.writeln;
+                stopButtonLight(a);
+                for(auto c = Call.min; c <= Call.max; c++){
+                    foreach(f; 0..4){
+                        callButtonLight(f, c, false);
+                    }
+                }
             },
         );
     }
